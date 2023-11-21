2654fc65fcc405c75f76de843abd5d41ba21b71d
OzziMrozi <112041313+OzziMrozi@users.noreply.github.com>
2023-11-21 08:12:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,51 +4,182 @@
 
 using namespace std;
 
-string imie;
-string odp;
-
+string name;
+string answer;
+int points = 0;
 int main()
 {
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
-    cin >> imie;
-    int pkt = 0;
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(handle, 11);
 
+    //Start---------------------------------------------------
+
+    cout << "What's your name: ";
+    cin >> name;
+    cout << "Hello! " << name << ", *topic* quiz will start in...";
+    Sleep(3500);
     system("cls");
+
+    //Counting----------------------------------------------
+
+    SetConsoleTextAttribute(handle, 12);
     cout<< "3";
     Sleep(1000);
     system("cls");
-    cout<< "3..2";
+    SetConsoleTextAttribute(handle, 6);
+    cout<< "2";
     Sleep(1000);
     system("cls");
-    cout<< "3..2..1..";
+    SetConsoleTextAttribute(handle, 10);
+    cout<< "1";
     Sleep(1000);
     system("cls");
+    SetConsoleTextAttribute(handle, 12);
+    cout<< "Start !!!";
+    Sleep(1300);
+    system("cls");
 
-    cout << imie << " witaj w tescie z programowania\n";
+    //Questions-------------------------------------------------
 
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
-    cin >> odp;
-    if(odp == "b")
+    SetConsoleTextAttribute(handle, 10);
+    cout << "First question: *question*."<<endl;
+    SetConsoleTextAttribute(handle, 15);
+    cout << "a) "<<endl;
+    cout << "b) "<<endl;
+    cout << "c) "<<endl;
+    SetConsoleTextAttribute(handle, 13);
+    cout << "\nYour answer: ";
+    cin >> answer;
+
+    if(answer == "b")
     {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
+        SetConsoleTextAttribute(handle, 10);
+        cout << "\nGood job ! You score 1 point";
+        points++;
     }
     else
     {
-        cout << "Zle! Jest to jezyk programowania!";
-
+        SetConsoleTextAttribute(handle, 12);
+        cout << "\nBad answer :/\nCorrect answer is: B";
     }
     Sleep(3000);
     system("cls");
 
+    SetConsoleTextAttribute(handle, 10);
+    cout << "Second question: *question*."<<endl;
+    SetConsoleTextAttribute(handle, 15);
+    cout << "a) "<<endl;
+    cout << "b) "<<endl;
+    cout << "c) "<<endl;
+    SetConsoleTextAttribute(handle, 13);
+    cout << "\nYour answer: ";
+    cin >> answer;
+    if(answer == "a")
+    {
+        SetConsoleTextAttribute(handle, 10);
+        cout << "\nGood job ! You score 1 point";
+        points++;
+    }
+    else
+    {
+        SetConsoleTextAttribute(handle, 12);
+        cout << "\nBad answer :/\nCorrect answer is: A";
+    }
+    Sleep(3000);
+    system("cls");
+
+    SetConsoleTextAttribute(handle, 10);
+    cout << "Third question: *question*."<<endl;
+    SetConsoleTextAttribute(handle, 15);
+    cout << "a) "<<endl;
+    cout << "b) "<<endl;
+    cout << "c) "<<endl;
+    SetConsoleTextAttribute(handle, 13);
+    cout << "\nYour answer: ";
+    cin >> answer;
+    if(answer == "b")
+    {
+        SetConsoleTextAttribute(handle, 10);
+        cout << "\nGood job ! You score 1 point";
+        points++;
+    }
+    else
+    {
+        SetConsoleTextAttribute(handle, 12);
+        cout << "\nBad answer :/\nCorrect answer is: B";
+    }
+    Sleep(3000);
+    system("cls");
+
+    SetConsoleTextAttribute(handle, 10);
+    cout << "Fourth question: *question*."<<endl;
+    SetConsoleTextAttribute(handle, 15);
+    cout << "a) "<<endl;
+    cout << "b) "<<endl;
+    cout << "c) "<<endl;
+    SetConsoleTextAttribute(handle, 13);
+    cout << "\nYour answer: ";
+    cin >> answer;
+    if(answer == "c")
+    {
+        SetConsoleTextAttribute(handle, 10);
+        cout << "\nGood job ! You score 1 point";
+        points++;
+    }
+    else
+    {
+        SetConsoleTextAttribute(handle, 12);
+        cout << "\nBad answer :/\nCorrect answer is: C";
+    }
+    Sleep(3000);
+    system("cls");
+
+    SetConsoleTextAttribute(handle, 10);
+    cout << "Fifth question: *question*."<<endl;
+    SetConsoleTextAttribute(handle, 15);
+    cout << "a) "<<endl;
+    cout << "b) "<<endl;
+    cout << "c) "<<endl;
+    SetConsoleTextAttribute(handle, 13);
+    cout << "\nYour annswer: ";
+    cin >> answer;
+    if(answer == "a")
+    {
+        SetConsoleTextAttribute(handle, 10);
+        cout << "\nGood job ! You score 1 point";
+        points++;
+    }else
+    {
+        SetConsoleTextAttribute(handle, 12);
+        cout << "\nBad answer :/\nCorrect answer is: A";
+    }
+    Sleep(3000);
+    system("cls");
+
+    //End---------------------------------------------------
+    SetConsoleTextAttribute(handle, 10);
+    for(int i=1; i<=3; i++)
+    {
+        Sleep(500);
+        system("cls");
+        cout << "!!! END OF QUIZ !!!";
+        Sleep(500);
+        system("cls");
+        cout << "    END OF QUIZ    ";
+    }
+    system("cls");
+
+    if(points = 1)
+    {
+        cout << "You scored: "<<points<<" point";
+    }else
+    {
+        cout << "You scored: "<<points<<" points";
+    }
+    
 
 
     return 0;
 }
+
+
